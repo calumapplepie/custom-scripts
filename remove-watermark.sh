c61acd5a12a00d9b552511d766f5b200cae0c234
Calum McConnell <calumlikesapplepie@gmail.com>
2024-02-19 12:40:49 -0500

    fixes for remove-watermark script

--- remove-watermark.sh
+++ remove-watermark.sh
@@ -10,6 +10,8 @@ WATERMARK=$1
 INBOUND=$2
 OUTBOUND=$3
 
+
+
 UNCOMPRESSED=`mktemp --dry-run 'uncompressed-XXXXXXXXXX.pdf'`
 FIXED=`mktemp --dry-run 'fixed-XXXXXXXXXX.pdf'`
 UNMARKED=`mktemp --dry-run 'unmarked-XXXXXXXXXX.pdf'`
@@ -18,7 +20,7 @@ WATERMARKLEN=${#WATERMARK}
 BLANKS=`printf %${WATERMARKLEN}s`
 
 qpdf --stream-data=uncompress "$INBOUND" $UNCOMPRESSED
-sed -e 's/\[($WATERMARK)\]/[($BLANKS)]/g' < $UNCOMPRESSED > $FIXED
+sed -e "s%$WATERMARK%$BLANKS%g" < $UNCOMPRESSED > $FIXED
 pdftk $FIXED output $UNMARKED
 qpdf --stream-data=compress $UNMARKED "$OUTBOUND"
 rm $UNCOMPRESSED $FIXED $UNMARKED
